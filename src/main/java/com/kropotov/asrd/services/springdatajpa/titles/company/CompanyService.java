77156eb040402659a4c9739f35f02cecae9333d1
shaitanshamma <kurbatov_andr@hotmail.com>
2020-05-25 20:29:28 +0300

    Исправил часть комментариев часть 2 Почистил companyOld,названия в множественных числах

--- src/main/java/com/kropotov/asrd/services/springdatajpa/titles/company/CompanyService.java
+++ src/main/java/com/kropotov/asrd/services/springdatajpa/titles/company/CompanyService.java
@@ -1,59 +1,40 @@
 package com.kropotov.asrd.services.springdatajpa.titles.company;
 
-import com.kropotov.asrd.converters.company.CompanyToDto;
-import com.kropotov.asrd.dto.company.CompanyDto;
 import com.kropotov.asrd.entities.company.Company;
 import com.kropotov.asrd.repositories.company.CompanyRepository;
-import com.kropotov.asrd.services.CrudService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class CompanyService implements CrudService<Company, Long> {
+public class CompanyService {
 
     private final CompanyRepository companyRepository;
-    private final CompanyToDto companyToDto;
 
-    public Optional<Company> getById(Long id) {
+    public Optional<Company> getById(Long id){
         return companyRepository.findById(id);
     }
 
-    @Override
-    public Optional<List<Company>> getAll() {
-        return Optional.ofNullable(companyRepository.findAll());
+    public List<Company> getAll(){
+        return companyRepository.findAll();
     }
 
-    public Page<Company> getAll(Pageable pageable) {
-        return companyRepository.findAll(pageable);
-    }
-
-    public Company getOneByTitle(String title) {
+    public Company getOneByTitle(String title){
         return companyRepository.findOneByTitle(title);
     }
 
-    @Transactional
-    public Company save(Company company) {
+    public Company save(Company company){
         return companyRepository.save(company);
     }
 
-    @Override
-    public void deleteById(Long id) {
+    public void delete(Long id){
         companyRepository.deleteById(id);
     }
 
-    public List<Company> getByMilitaryRepresentation(String militaryRepresentation) {
+    public List<Company> getByMilitaryRepresentation(String militaryRepresentation){
         return companyRepository.findByMilitaryRepresentation(militaryRepresentation);
-    }
-
-    @Transactional
-    public CompanyDto getDtoById(Long id) {
-        return companyToDto.convert(getById(id).orElse(new Company()));
     }
 }
