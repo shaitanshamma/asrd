162b62addc05bc77be82478392579e49600c85a2
sportarty <31653016+sportarty@users.noreply.github.com>
2020-05-30 22:14:03 +0300

    Revert "Сервис отправки пользователям уведомлений по email"

--- src/main/java/com/kropotov/asrd/services/email/MessageService.java
+++
@@ -1,9 +0,0 @@
-package com.kropotov.asrd.services.email;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface MessageService<T extends Message> {
-
-    void sendMessage(T message);
-}
