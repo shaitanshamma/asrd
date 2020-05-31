2237ff5ab0bcf09e28f8576b19a3f57b2d77a705
Dmitrenko <Dias64@mail.ru>
2020-05-31 23:05:48 +0300

    Создал абстрактный класс Message с полями, которые есть у любого сообщения

---
+++ src/main/java/com/kropotov/asrd/services/email/Message.java
@@ -0,0 +1,14 @@
+package com.kropotov.asrd.services.email;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Message {
+
+    private String messageFrom; // от кого (адрес почтового ящика)
+    private String messageContent;  // содержание письма
+    private String messageTo;   // кому (адрес почтового ящика)
+
+}
