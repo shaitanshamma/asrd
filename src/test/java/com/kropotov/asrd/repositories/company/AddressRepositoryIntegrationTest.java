ac20fe57857444a96b0c7d03609b00c2f75c8981
shaitanshamma <kurbatov_andr@hotmail.com>
2020-06-09 19:32:07 +0300

    Company. Поправил тесты согласно замечаниям

--- src/test/java/com/kropotov/asrd/repositories/company/AddressRepositoryIntegrationTest.java
+++ src/test/java/com/kropotov/asrd/repositories/company/AddressRepositoryIntegrationTest.java
@@ -2,6 +2,7 @@ package com.kropotov.asrd.repositories.company;
 
 import com.kropotov.asrd.entities.company.Address;
 import com.kropotov.asrd.entities.company.Company;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,35 +15,23 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class AddressRepositoryIntegrationTest {
+
+    private List<Address> addresses = new ArrayList<>();
+
+    private Company company = new Company();
 
     @Autowired
     private TestEntityManager entityManager;
 
     @Autowired
     private AddressRepository addressRepository;
+    @Before
+    public void setUp() {
 
-    @Test
-    public void whenFindAll_thenReturnListAddresses() {
-        List<Address> addresses = new ArrayList<>();
-        for (int i = 0; i <5 ; i++) {
-            Address address_i = new Address();
-            addresses.add(address_i);
-            entityManager.persist(address_i);
-        }
-        entityManager.flush();
-
-        List<Address> found = addressRepository.findAll();
-
-        assertThat(found).isEqualTo(addresses);
-    }
-
-    @Test
-    public void whenFindAllByCompany_thenReturnListAddresses() {
-        List<Address> addresses = new ArrayList<>();
-        Company company = new Company();
         for (int i = 0; i <5 ; i++) {
             Address address_i = new Address();
             address_i.setCompany(company);
@@ -51,6 +40,17 @@ public class AddressRepositoryIntegrationTest {
             entityManager.persist(company);
         }
         entityManager.flush();
+    }
+    @Test
+    public void whenFindAll_thenReturnListAddresses() {
+
+        List<Address> found = addressRepository.findAll();
+
+        assertThat(found).isEqualTo(addresses);
+    }
+
+    @Test
+    public void whenFindAllByCompany_thenReturnListAddresses() {
 
         List<Address> found = addressRepository.findAllByCompany(company);
 
--- src/test/java/com/kropotov/asrd/repositories/company/CompanyPhoneRepositoryIntegrationTest.java
+++ src/test/java/com/kropotov/asrd/repositories/company/CompanyPhoneRepositoryIntegrationTest.java
@@ -1,8 +1,8 @@
 package com.kropotov.asrd.repositories.company;
 
-import com.kropotov.asrd.entities.company.Address;
 import com.kropotov.asrd.entities.company.Company;
 import com.kropotov.asrd.entities.company.CompanyPhone;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,21 +19,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest
 public class CompanyPhoneRepositoryIntegrationTest {
 
+    private List<CompanyPhone> companyPhones = new ArrayList<>();
+
+    private Company company = new Company();
+
     @Autowired
     private TestEntityManager entityManager;
 
     @Autowired
     private CompanyPhoneRepository companyPhoneRepository;
 
-    @Test
-    public void whenFindAll_thenReturnListCompanyPhones() {
-        List<CompanyPhone> companyPhones = new ArrayList<>();
-        for (int i = 0; i <5 ; i++) {
+    @Before
+    public void setUp() {
+
+        for (int i = 0; i < 5; i++) {
             CompanyPhone companyPhone_i = new CompanyPhone();
             companyPhones.add(companyPhone_i);
             entityManager.persist(companyPhone_i);
+            entityManager.persist(company);
         }
         entityManager.flush();
+    }
+
+    @Test
+    public void whenFindAll_thenReturnListCompanyPhones() {
 
         List<CompanyPhone> found = companyPhoneRepository.findAll();
 
@@ -42,15 +51,6 @@ public class CompanyPhoneRepositoryIntegrationTest {
 
     @Test
     public void whenFindAllByCompany_thenReturnListCompanyPhones() {
-        List<CompanyPhone> companyPhones = new ArrayList<>();
-        Company company = new Company();
-        for (int i = 0; i <5 ; i++) {
-            CompanyPhone companyPhone_i = new CompanyPhone();
-            companyPhones.add(companyPhone_i);
-            entityManager.persist(companyPhone_i);
-            entityManager.persist(company);
-        }
-        entityManager.flush();
 
         List<CompanyPhone> found = companyPhoneRepository.findAll();
 
